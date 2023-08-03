d8c7a248a2213dc528ca70173d019e73e1dd099e
aresKostroma <Aurimasu@mail.ru>
2023-08-03 20:22:46 +0300

    add file

---
+++ 24.7.2.py
@@ -0,0 +1,66 @@
+import requests
+import json
+
+class PetFriends:
+    def __init__(self):
+        self.base_url = "https: // petfriends.skillfactory.ru / apidocs /"
+
+    def get_api_key(self, email, password):
+
+        headers = {
+            'email': email,
+            'password': password
+        }
+        res = requests.get(self.base_url+'api/key', headers=headers)
+        status = res.status_code
+        result = ''
+        try:
+            result = res.json()
+        except:
+            result = res.text
+        return status, result
+
+    def get_list_of_pets(self, auth_key, filter):
+        headers = {'auth_key': auth_key['key']}
+        filter = {'filter': filter}
+
+        res = requests.get(self.base_url+'api/pets', headers=headers, params=filter)
+        status = res.status_code
+        result = ''
+        try:
+            result = res.json()
+        except:
+            result = res.text
+        return status, result
+
+    def delete_pet(self, auth_key: json, pet_id: str) -> json:
+
+        headers = {'auth_key': auth_key['key']}
+
+        res = requests.delete(self.base_url + 'api/pets/' + pet_id, headers=headers)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
+
+    def update_pet_info(self, auth_key: json, pet_id: str, name: str,
+                        animal_type: str, age: int) -> json:
+
+        headers = {'auth_key': auth_key['key']}
+        data = {
+            'name': name,
+            'age': age,
+            'animal_type': animal_type
+        }
+
+        res = requests.put(self.base_url + 'api/pets/' + pet_id, headers=headers, data=data)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
